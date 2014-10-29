e4a409752f53bfe128d48ac1c0aec9c689dee2ae
Shaquille <defacid@gmail.com>
2014-10-29 09:29:12 -0400

    Create script.js

---
+++ test/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	$('a[href^="#"]').on('click',function (e) {
+
+	    e.preventDefault();
+
+	    var target = this.hash,
+
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+
+	        'scrollTop': $target.offset().top
+
+	    }, 700, 'swing', function () {
+
+	        window.location.hash = target;
+
+	    });
+
+	});
+
+});
